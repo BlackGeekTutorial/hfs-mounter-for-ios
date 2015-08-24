ffae8734953e83cda301a26449149e4ceea61157
Andrea Bentivegna <BlackGeek@iMac-di-Andrea.station>
2015-08-24 18:09:07 +0200

    fixed std err reading; added verbose output

--- hfsmounter.sh
+++ hfsmounter.sh
@@ -1,54 +1,78 @@
 #!/bin/bash
 
-echo ""; echo "Welcome to hfs mounter for iOS"; echo ""
+echo ""; echo "Welcome to hfs mounter for iOS"
 
-echo "Processing default iOS mount path..."
-for d in /var/mnt/mount*
-do 
-	umount $d > /dev/null 2>&1
-done
-
-echo "Checking if this is a developer device..."
-DEVELOPER="$(df /Developer)"
-if [[ $DEVELOPER == *"disk0"* ]]; then
-	echo "Your device is not in developer mode."
-	developermode=false
-else
-	echo "Developer partition detected. You're device is in developer mode."
-	developermode=true
+if ! [ "$1" == "-v" ]; then
+    echo "Use -v argument for verbose output"
 fi
 
-echo "Creating mounting point..."; echo ""
+echo ""
+
+if [ "$1" == "-v" ]; then
+    echo "Processing default iOS mount path..."
+fi
+
+for d in /var/mnt/mount*; do
+    umount $d > /dev/null 2>&1
+done
+
+if [ "$1" == "-v" ]; then
+    echo "Checking if this is a developer device..."
+fi
+
+DEVELOPER="$(df /Developer)"
+if [[ $DEVELOPER == *"disk0"* ]]; then
+    if [ "$1" == "-v" ]; then
+        echo "Your device is not in developer mode."
+    fi
+    developermode=false
+else
+    if [ "$1" == "-v" ]; then
+        echo "Developer partition detected. You're device is in developer mode."
+    fi
+    developermode=true
+fi
+
+if [ "$1" == "-v" ]; then
+    echo "Creating mounting point..."; echo ""
+fi
+
 mkdir "/var/mobile/EXT" > /dev/null 2>&1
 umount "/var/mobile/EXT" > /dev/null 2>&1
 
-for i in /dev/disk*;
-do
-
-	echo "Checking disk: $i ..."
+for i in /dev/disk*; do
+    if [ "$1" == "-v" ]; then
+        echo "Checking disk: $i ..."
+    fi
    
-	if [ "$developermode" = true ] ; then
-		if [[ $DEVELOPER == *"$i"* ]]; then
-			echo "This device is used by Xcode. Skipping..."; echo ""
+    if [ "$developermode" = true ] ; then
+        if [[ $DEVELOPER == *"$i"* ]]; then
+            if [ "$1" == "-v" ]; then
+                echo "This device is used by Xcode. Skipping..."; echo ""
+            fi
 continue
-		fi
-	fi
+        fi
+    fi
    
-	if mount | grep "$i" > /dev/null; then
-		echo "This device is used by the system. Skipping..."; echo ""
+    if mount | grep "$i" > /dev/null; then
+        if [ "$1" == "-v" ]; then
+            echo "This device is used by the system. Skipping..."; echo ""
+        fi
 continue
-	fi
+    fi
 
-	mount_hfs "$i" "/var/mobile/EXT" > /dev/null 2>&1
+    mount_hfs "$i" "/var/mobile/EXT" > /dev/null 2>&1
    
-	MOUNTPOINT="$(df /var/mobile/EXT)"
+    MOUNTPOINT="$(df /var/mobile/EXT)"
    
-	if ! [[ $MOUNTPOINT == *"disk0"* ]]; then
-		echo ""; echo ""
-		echo -en '\E[47;32m'"\033[1m>>> Device has been successfully mounted in: '/var/mobile/EXT' <<<\033[0m"; echo ""
+    if ! [[ $MOUNTPOINT == *"disk0"* ]]; then
+        echo ""; echo ""
+        echo -en '\E[47;32m'"\033[1m>>> Device has been successfully mounted in: '/var/mobile/EXT' <<<\033[0m"; echo ""
 break
-	else
-    	echo "Not the right one. Skipping..."; echo ""
-	fi
+    else
+        if [ "$1" == "-v" ]; then
+            echo "Not the right one. Skipping..."; echo ""
+        fi
+    fi
 	
 done
--- hfsunmounter.sh
+++ hfsunmounter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UNMOUNTRESULT="$(umount /private/var/mobile/EXT)"
+UNMOUNTRESULT=$( { umount /private/var/mobile/EXT | sed s/Output/Useless/ > outfile; } 2>&1 )
 
 if [[ $UNMOUNTRESULT == *"not currently mounted"* ]]; then
 	echo "Device already unmounted :)"
