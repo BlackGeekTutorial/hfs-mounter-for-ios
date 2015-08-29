3bc9e76e48e19a4d0413019d3efebf2d1504fc86
Andrea Bentivegna <BlackGeek@iMac-di-Andrea.station>
2015-08-29 12:01:01 +0200

    improved verbose output

--- usr/bin/hfsmounter.sh
+++ usr/bin/hfsmounter.sh
@@ -4,13 +4,17 @@
 # Just plug your USB/SD Card in with the Apple Camera Connection Kit and wait for the "Unsupported device" message on your screen. Then, run 'hfsmounter.sh' from root user in Terminal.
 
 iscmdfound () {
-
 CLITOOL=$( { $1 | sed s/Output/Useless/ > outfile; } 2>&1 )
 if [[ $CLITOOL == *"command not found"* ]]; then
     echo "ERROR: Command not found: '$1'. Exiting with failure status now."
     exit 1
 fi
+}
 
+show () {
+if [ "$1" == "-v" ]; then
+    echo "$2"
+fi
 }
 
 iscmdfound df
@@ -24,80 +28,56 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-
 echo ""; echo "Welcome to hfs mounter for iOS"
-
-if ! [ "$1" == "-v" ]; then
-    echo "Use -v argument for verbose output"
-fi
-
+show "$1" "Use -v argument for verbose output"
 echo ""
+show "$1" "Processing default iOS mount path..."
 
-if [ "$1" == "-v" ]; then
-    echo "Processing default iOS mount path..."
-fi
-
-for d in /var/mnt/mount*; do
+for d in /private/var/mnt/mount*; do
     umount $d > /dev/null 2>&1
 done
 
-if [ "$1" == "-v" ]; then
-    echo "Checking if this is a developer device..."
-fi
+show "$1" "Checking if this is a developer device..."
 
 DEVELOPER="$(df /Developer)"
 if [[ $DEVELOPER == *"disk0"* ]]; then
-    if [ "$1" == "-v" ]; then
-        echo "Your device is not in developer mode."
-    fi
+    show "$1" "Your device is not in developer mode."
     developermode=false
 else
-    if [ "$1" == "-v" ]; then
-        echo "Developer partition detected. You're device is in developer mode."
-    fi
+    show "$1" "Developer partition detected. You're device is in developer mode."
     developermode=true
 fi
 
-if [ "$1" == "-v" ]; then
-    echo "Creating mounting point..."; echo ""
-fi
+show "$1" "Creating mounting point..."; show "$1" ""
 
 mkdir "/var/mobile/EXT" > /dev/null 2>&1
 umount "/var/mobile/EXT" > /dev/null 2>&1
 
 for i in /dev/disk*; do
-    if [ "$1" == "-v" ]; then
-        echo "Checking disk: $i ..."
-    fi
+    show "$1" "Checking disk: $i ..."
    
     if [ "$developermode" = true ] ; then
         if [[ $DEVELOPER == *"$i"* ]]; then
-            if [ "$1" == "-v" ]; then
-                echo "This device is used by Xcode. Skipping..."; echo ""
-            fi
+            show "$1" "This device is used by Xcode. Skipping..."; show "$1" ""
 continue
         fi
     fi
    
     if mount | grep "$i" > /dev/null; then
-        if [ "$1" == "-v" ]; then
-            echo "This device is used by the system. Skipping..."; echo ""
-        fi
+        show "$1" "This device is used by the system. Skipping..."; show "$1" ""
 continue
     fi
 
     mount_hfs "$i" "/var/mobile/EXT" > /dev/null 2>&1
    
-    MOUNTPOINT="$(df /var/mobile/EXT)"
+    MOUNTPOINT="$(df /private/var/mobile/EXT)"
    
     if ! [[ $MOUNTPOINT == *"disk0"* ]]; then
         echo ""; echo ""
         echo -en '\E[47;32m'"\033[1m>>> Device has been successfully mounted in: '/var/mobile/EXT' <<<\033[0m"; echo ""
 break
     else
-        if [ "$1" == "-v" ]; then
-            echo "Not the right one. Skipping..."; echo ""
-        fi
+        show "$1" "Not the right one. Skipping..."; show "$1" ""
     fi
 	
 done
